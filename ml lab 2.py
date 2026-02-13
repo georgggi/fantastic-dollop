4a169a268c80da334066314ddafa4d3c3ccb2b0f
Ajnura <orozalievkubat7@gmail.com>
2026-02-14 02:14:04 +0600

    add

--- ml lab 2.py
+++ ml lab 2.py
@@ -1,13 +1,15 @@
 import pandas as pd
-
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_theme()
 df = pd.read_csv('titanic.csv')
-'''total_salary = df['Monthly_Salary'].sum()
-average_salary = df['Monthly_Salary'].mean()
-print('Total monthly salary: ', total_salary)
-print('Average monthly salary: ', average_salary)'''
-print(df.head())
-print(df.info())
-print(df.describe())
-print(df.hist)
-print(df.boxplot)
-print(df.corr)
+
+print(df.select_dtypes(int).describe())
+'''print(df['Age'].hist(bins=50, figsize=(10, 5)))
+print(df.boxplot(column='Age', figsize=(10, 4)))
+print(df.plot.scatter(x='Age', y='Survived'))
+print(df.select_dtypes(int).corr())
+sns.histplot(df['Age'], bins=50)'''
+sns.scatterplot(x='Age', y='Survived', data=df)
+plt.show()
