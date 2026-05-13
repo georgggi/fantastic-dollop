d6016e5c06f5485b22ab6981bbd0bc2639d392b8
Ajnura <orozalievkubat7@gmail.com>
2026-05-13 22:45:41 +0600

    add

--- rest.py
+++ rest.py
@@ -2,4 +2,9 @@ import pandas as pd
 
 df = pd.read_csv('restaurants.csv')
 
+df.loc[df['cuisine'] == 'Кыргызская', 'cuisine'] = 'Национальная'
+
+df.to_csv('restaurant1.csv',
+        index=False,
+        encoding="utf-8-sig")
 print(df)
